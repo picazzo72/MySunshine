453f33bf9c3b4e1f9f81421f961ed99a1267636b
Dan Steen Andersen <dandersen00@icloud.com>
2016-05-05 12:08:26 +0200

    Preferred location be shown on a map using the "Open location" menu using an implicit intent

--- app/src/main/java/com/example/dandersen/my_sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/dandersen/my_sunshine/app/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.dandersen.my_sunshine.app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +45,35 @@ public class MainActivity extends AppCompatActivity {
             startActivity(openSettingsActivityIntent);
             return true;
         }
+        else if (id == R.id.action_view_preferred_location) {
+            openPreferredLocationInMap();
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        // Get location from settings
+        String location = PreferenceManager
+                .getDefaultSharedPreferences(this)
+                .getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+
+        // Build geo location
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        Log.v(LOG_TAG, "Map url: " + geoLocation.toString());
+
+        // Create implicit intent
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        mapIntent.setData(geoLocation);
+
+        // Verify that the intent will resolve to an activity
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+        else {
+            Toast.makeText(this, getString(R.string.error_msg_no_map), Toast.LENGTH_SHORT).show();
+        }
     }
 }
